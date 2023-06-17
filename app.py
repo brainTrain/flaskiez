23f753d231570b7a8b1b1974162b7145a766d4d4
brainTrain <abductee@gmail.com>
2023-06-16 19:58:10 -0700

    add a route that fetches from an external api

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+import urllib.request
+
 from dotenv import dotenv_values
 from flask import Flask, request
 
@@ -12,21 +15,27 @@ ROOT_URL = ENV.get("ROOT_URL", "/")
 HOME_ROUTE_NAME = ""
 BRUH_ROUTE_NAME = "bruh"
 SHOOT_ROUTE_NAME = "shoot"
+QUOTE_ROUTE_NAME = "quote"
 HOME_ROUTE = "{root}{url}".format(url=HOME_ROUTE_NAME, root=ROOT_URL)
 BRUH_ROUTE = "{root}{url}".format(url=BRUH_ROUTE_NAME, root=ROOT_URL)
 SHOOT_ROUTE = "{root}{url}".format(url=SHOOT_ROUTE_NAME, root=ROOT_URL)
+QUOTE_ROUTE = "{root}{url}".format(url=QUOTE_ROUTE_NAME, root=ROOT_URL)
 
 
 @app.route(HOME_ROUTE)
 def hello_world():
     return (
-        "<main><p>Hello, World!</p> <section><a"
-        " href={bruh_url}>{bruh_name}</a></section> <section><a"
-        " href={shoot_url}?name=oic>{shoot_name}</a></section> </main>".format(
+        "<main><p>Hello, World!</p>"
+        "<section><a href={bruh_url}>{bruh_name}</a></section>"
+        "<section><a href={shoot_url}?name=oic>{shoot_name}</a></section>"
+        "<section><a href={quote_url}>{quote_name}</a></section>"
+        "</main>".format(
             bruh_url=BRUH_ROUTE,
             bruh_name=BRUH_ROUTE_NAME,
             shoot_name=SHOOT_ROUTE_NAME,
             shoot_url=SHOOT_ROUTE,
+            quote_url=QUOTE_ROUTE,
+            quote_name=QUOTE_ROUTE_NAME,
         )
     )
 
@@ -51,3 +60,13 @@ def shoot():
             " asdfasdfasdfasdfsdfasdfasdfasdfasdfasdf".format(name=name)
         )
     }
+
+
+@app.route(QUOTE_ROUTE)
+def quote():
+    url = "https://catfact.ninja/fact"
+    response = urllib.request.urlopen(url)
+    data_response = response.read()
+    data = json.loads(data_response)
+
+    return data
