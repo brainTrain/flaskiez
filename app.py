d27af946674e663c6db7109a2e4314fd53c32772
brainTrain <abductee@gmail.com>
2023-06-16 19:09:56 -0700

    parse query paramzz

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 HOME_ROUTE_NAME = ""
 BRUH_ROUTE_NAME = "bruh"
+SHOOT_ROUTE_NAME = "shoot"
 HOME_ROUTE = "/{url}".format(url=HOME_ROUTE_NAME)
 BRUH_ROUTE = "/{url}".format(url=BRUH_ROUTE_NAME)
+SHOOT_ROUTE = "/{url}".format(url=SHOOT_ROUTE_NAME)
 
 
 @app.route(HOME_ROUTE)
 def hello_world():
-    return "<main><p>Hello, World!</p><a href={url}>{name}</a></main>".format(
-        url=BRUH_ROUTE, name=BRUH_ROUTE_NAME
+    return (
+        "<main><p>Hello, World!</p> <section><a"
+        " href={bruh_url}>{bruh_name}</a></section> <section><a"
+        " href={shoot_url}?name=oic>{shoot_name}</a></section> </main>".format(
+            bruh_url=BRUH_ROUTE,
+            bruh_name=BRUH_ROUTE_NAME,
+            shoot_name=SHOOT_ROUTE_NAME,
+            shoot_url=SHOOT_ROUTE,
+        )
     )
 
 
@@ -22,5 +31,16 @@ def oh():
             "no--------- 00000000 0000000000"
             "0000000000 ooooooooooooooooooooooooooo"
             " oooooooooooooooooooooooo"
+        )
+    }
+
+
+@app.route(SHOOT_ROUTE)
+def shoot():
+    name = request.args.get("name")
+    return {
+        "hai": (
+            "u guyaii!! ____----{name}----____ fasdfasdfasdfasdf"
+            " asdfasdfasdfasdfsdfasdfasdfasdfasdfasdf".format(name=name)
         )
     }
